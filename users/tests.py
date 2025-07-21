8e2867fb16c698ac5ad8a66b6fca9b06958d6602
Nazmus Sakib <nsak0012@student.monash.edu>
2025-07-21 20:24:55 +1000

    Added test unit for register

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class UserModelTests(TestCase):
+    def test_create_user(self):
+        user = User.objects.create_user(
+            username='testuser',
+            email='test@example.com',
+            password='testpass123'
+        )
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.email, 'test@example.com')
+        self.assertTrue(user.check_password('testpass123'))
